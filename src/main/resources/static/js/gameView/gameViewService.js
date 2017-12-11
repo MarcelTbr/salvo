151ec2a40f890ee8274384901be5a9fab7d62c90
Marcel Tauber <marceltbr@gmail.com>
2017-12-11 12:13:33 +0100

    Auth fix

--- src/main/resources/static/js/gameView/gameViewService.js
+++ src/main/resources/static/js/gameView/gameViewService.js
@@ -11,9 +11,9 @@ angular.module('PlayerViewModule').service('updateGameView', function($http, $wi
             }).error(function(response){
 
                  console.log("Response:"); console.log(response);
-                alert("Sorry, not authorized to see this page.")
+               // alert("Sorry, not authorized to see this page.")
 
-                $window.location.href = "http://" + $window.location.host + "/games.html";
+                //$window.location.href = "http://" + $window.location.host + "/games.html";
 
             });
 
